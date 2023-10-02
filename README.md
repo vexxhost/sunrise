8b4a726d9481a9bcd1da6ae5b9206191baba0097
Ed Timmons <ed@delsurf.com>
2023-10-02 14:37:12 -0400

    use tailwindui and nextui to apply layout and sample table

--- README.md
+++ README.md
@@ -5,31 +5,23 @@
 1. Create a client inside Keycloak which will be used for authenticating
    the user using Sunrise.
 
-1. Create a file called `.env.local` in the root of the project and add the
-   following variables (refer to the values as examples):
+2. Make a copy of the file `.env.dist` and rename the copy to `.env.local` in the root of the project and update the
+   variables (refer to the values as examples):
 
-   ```bash
-   NODE_TLS_REJECT_UNAUTHORIZED=0 # if running against an self-signed certs.
-   KEYSTONE_API=https://identity.cloud.atmosphere.dev
-   KEYCLOAK_ISSUER=https://keycloak.cloud.atmosphere.dev/realms/atmosphere
-   KEYCLOAK_CLIENT_ID=sunrise
-   KEYCLOAK_CLIENT_SECRET=secret123
-   ```
-
-1. Start the development server
+3. Start the development server
 
    ```bash
    npm run dev
    ```
 
-1. Navigate to the following URL and login with Keycloak:
+4. Navigate to the following URL and login with Keycloak:
 
    [http://localhost:3000/api/auth/signin](http://localhost:3000/api/auth/signin)
 
-1. Go to the following URL to see the user's information:
+5. Go to the following URL to see the user's information:
 
-   [http://localhost:3000/compute/instances](http://localhost:3000/compute/instances)
+   [http://localhost:3000/protected](http://localhost:3000/protected)
 
-1. Go to the following URL to signout:
+6. Go to the following URL to signout:
 
    [http://localhost:3000/api/auth/signout](http://localhost:3000/api/auth/signout)
---
+++ components/MainWrapper.tsx
@@ -0,0 +1,362 @@
+"use client"
+
+import { Fragment, useState } from 'react'
+import Image from 'next/image'
+import { Dialog, Disclosure, Listbox, Transition } from '@headlessui/react'
+import {
+  Bars3Icon,
+  CheckIcon,
+  ChevronUpDownIcon,
+  ChevronRightIcon,
+  Cog6ToothIcon,
+  XMarkIcon,
+} from '@heroicons/react/24/outline'
+import SearchBar from '@/components/MainWrapper/SearchBar'
+import Profile from '@/components/MainWrapper/Profile'
+import { navigation } from '@/lib/navigation'
+
+const projects = [{
+  id: 1,
+  name: 'Project Alpha',
+  unavailable: false
+},
+{
+  id: 2,
+  name: 'Project Beta',
+  unavailable: false
+}]
+
+function classNames(...classes:any) {
+  return classes.filter(Boolean).join(' ')
+}
+
+export default function MainWrapper({ children }:any) {
+  const [sidebarOpen, setSidebarOpen] = useState(false)
+  const [selectedProject, setSelectedProject] = useState(projects[0]);
+
+  return (
+    <>
+      <div>
+        <Transition.Root show={sidebarOpen} as={Fragment}>
+          <Dialog as="div" className="relative z-50 lg:hidden" onClose={setSidebarOpen}>
+            <Transition.Child
+              as={Fragment}
+              enter="transition-opacity ease-linear duration-300"
+              enterFrom="opacity-0"
+              enterTo="opacity-100"
+              leave="transition-opacity ease-linear duration-300"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <div className="fixed inset-0 bg-gray-900/80" />
+            </Transition.Child>
+
+            <div className="fixed inset-0 flex">
+              <Transition.Child
+                as={Fragment}
+                enter="transition ease-in-out duration-300 transform"
+                enterFrom="-translate-x-full"
+                enterTo="translate-x-0"
+                leave="transition ease-in-out duration-300 transform"
+                leaveFrom="translate-x-0"
+                leaveTo="-translate-x-full"
+              >
+                <Dialog.Panel className="relative mr-16 flex w-full max-w-xs flex-1">
+                  <Transition.Child
+                    as={Fragment}
+                    enter="ease-in-out duration-300"
+                    enterFrom="opacity-0"
+                    enterTo="opacity-100"
+                    leave="ease-in-out duration-300"
+                    leaveFrom="opacity-100"
+                    leaveTo="opacity-0"
+                  >
+                    <div className="absolute left-full top-0 flex w-16 justify-center pt-5">
+                      <button type="button" className="-m-2.5 p-2.5" onClick={() => setSidebarOpen(false)}>
+                        <span className="sr-only">Close sidebar</span>
+                        <XMarkIcon className="h-6 w-6 text-white" aria-hidden="true" />
+                      </button>
+                    </div>
+                  </Transition.Child>
+                  {/* Sidebar component, swap this element with another sidebar if you like */}
+                  <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-gray-900 px-6 pb-4 ring-1 ring-white/10">
+                    <div className="flex h-16 shrink-0 items-center">
+                      <a href="/"><Image
+                        className="h-8 w-auto"
+                        src="/img/logo.png"
+                        alt="Sunrise"
+                        width="1225"
+                        height="189"
+                      /></a>
+                    </div>
+                    <div className="-mx-2">
+                      <Listbox value={selectedProject} onChange={setSelectedProject}>
+                        {({ open }) => (
+                          <>
+                            <div className="relative">
+                              <Listbox.Button className="relative w-full cursor-default rounded-md bg-gray-900 py-1.5 pl-3 pr-10 text-left text-white shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-gray sm:text-sm sm:leading-6">
+                                <span className="block truncate">{selectedProject.name}</span>
+                                <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+                                  <ChevronUpDownIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
+                                </span>
+                              </Listbox.Button>
+
+                              <Transition
+                                show={open}
+                                as={Fragment}
+                                leave="transition ease-in duration-100"
+                                leaveFrom="opacity-100"
+                                leaveTo="opacity-0"
+                              >
+                                <Listbox.Options className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                                  {projects.map((project) => (
+                                    <Listbox.Option
+                                      key={project.id}
+                                      className={({ active }) =>
+                                        classNames(
+                                          active ? 'bg-gray-900 text-white' : 'text-gray-900',
+                                          'relative cursor-default select-none py-2 pl-3 pr-9'
+                                        )
+                                      }
+                                      value={project}
+                                    >
+                                      {({ selected, active }) => (
+                                        <>
+                                          <span className={classNames(selected ? 'font-semibold' : 'font-normal', 'block truncate')}>
+                                            {project.name}
+                                          </span>
+
+                                          {selected ? (
+                                            <span
+                                              className={classNames(
+                                                active ? 'text-white bg-gray-900' : 'text-gray-900',
+                                                'absolute inset-y-0 right-0 flex items-center pr-4'
+                                              )}
+                                            >
+                                              <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                                            </span>
+                                          ) : null}
+                                        </>
+                                      )}
+                                    </Listbox.Option>
+                                  ))}
+                                </Listbox.Options>
+                              </Transition>
+                            </div>
+                          </>
+                        )}
+                      </Listbox>
+                    </div>
+                    <nav className="flex flex-1 flex-col mt-4">
+                      <ul role="list" className="flex flex-1 flex-col gap-y-7">
+                        <li>
+                          <ul role="list" className="-mx-2 space-y-1">
+                            {navigation.map((item) => (
+                              <li key={item.name}>
+                                <a
+                                  href={item.href}
+                                  className={classNames(
+                                    item.current
+                                      ? 'bg-gray-100 text-gray-900'
+                                      : 'text-gray-400 hover:text-gray-900 hover:bg-gray-100',
+                                    'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold'
+                                  )}
+                                >
+                                  <item.icon className="h-6 w-6 shrink-0" aria-hidden="true" />
+                                  {item.name}
+                                </a>
+                              </li>
+                            ))}
+                          </ul>
+                        </li>
+                        <li className="mt-auto">
+                          <a
+                            href="#"
+                            className="group -mx-2 flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6 text-gray-400 hover:bg-yellow hover:text-white"
+                          >
+                            <Cog6ToothIcon className="h-6 w-6 shrink-0" aria-hidden="true" />
+                            Settings
+                          </a>
+                        </li>
+                      </ul>
+                    </nav>
+                  </div>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </Dialog>
+        </Transition.Root>
+
+        {/* Static sidebar for desktop */}
+        <div className="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-72 lg:flex-col">
+          {/* Sidebar component, swap this element with another sidebar if you like */}
+          <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-gray-900 px-6 pb-4">
+            <div className="flex h-16 shrink-0 items-center">
+              <a href="/"><Image
+                className="h-8 w-auto"
+                src="/img/logo.png"
+                alt="Sunrise"
+                width="1225"
+                height="189"
+              /></a>
+            </div>
+            <div className="-mx-2">
+              <Listbox value={selectedProject} onChange={setSelectedProject}>
+                {({ open }) => (
+                  <>
+                    <div className="relative">
+                      <Listbox.Button className="relative w-full cursor-default rounded-md bg-dark-gray py-1.5 pl-3 pr-10 text-left text-white shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-gray-900 sm:text-sm sm:leading-6">
+                        <span className="block truncate">{selectedProject.name}</span>
+                        <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+                          <ChevronUpDownIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
+                        </span>
+                      </Listbox.Button>
+
+                      <Transition
+                        show={open}
+                        as={Fragment}
+                        leave="transition ease-in duration-100"
+                        leaveFrom="opacity-100"
+                        leaveTo="opacity-0"
+                      >
+                        <Listbox.Options className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                          {projects.map((project) => (
+                            <Listbox.Option
+                              key={project.id}
+                              className={({ active }) =>
+                                classNames(
+                                  active ? 'bg-gray-900 text-white' : 'text-gray-900',
+                                  'relative cursor-default select-none py-2 pl-3 pr-9'
+                                )
+                              }
+                              value={project}
+                            >
+                              {({ selected, active }) => (
+                                <>
+                                  <span className={classNames(selected ? 'font-semibold' : 'font-normal', 'block truncate')}>
+                                    {project.name}
+                                  </span>
+
+                                  {selected ? (
+                                    <span
+                                      className={classNames(
+                                        active ? 'text-white' : 'text-gray-900',
+                                        'absolute inset-y-0 right-0 flex items-center pr-4'
+                                      )}
+                                    >
+                                      <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                                    </span>
+                                  ) : null}
+                                </>
+                              )}
+                            </Listbox.Option>
+                          ))}
+                        </Listbox.Options>
+                      </Transition>
+                    </div>
+                  </>
+                )}
+              </Listbox>
+            </div>
+            <nav className="flex flex-1 flex-col mt-4">
+              <ul role="list" className="flex flex-1 flex-col gap-y-7">
+                <li>
+                  <ul role="list" className="-mx-2 space-y-1">
+                    {navigation.map((item) => (
+                      <li key={item.name}>
+                        {!item.children ? (
+                          <a
+                            href={item.href}
+                            className={classNames(
+                              item.current
+                                ? 'bg-gray-100 text-gray-900'
+                                : 'text-gray-400 hover:text-gray-900 hover:bg-gray-100',
+                              'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold'
+                            )}
+                          >
+                            <item.icon className="h-6 w-6 shrink-0" aria-hidden="true" />
+                            {item.name}
+                          </a>
+                        ) : (
+                          <Disclosure as="div">
+                            {({ open }) => (
+                              <>
+                                <Disclosure.Button
+                                  className={classNames(
+                                    item.current ? 'bg-gray-50' : 'hover:bg-gray-100 hover:text-gray-900',
+                                    'flex items-center w-full text-left rounded-md p-2 gap-x-3 text-sm leading-6 font-semibold text-gray-400'
+                                  )}
+                                >
+                                  <item.icon className="h-6 w-6 shrink-0 text-gray-400" aria-hidden="true" />
+                                  {item.name}
+                                  <ChevronRightIcon
+                                    className={classNames(
+                                      open ? 'rotate-90 text-gray-500' : 'text-gray-400',
+                                      'ml-auto h-5 w-5 shrink-0'
+                                    )}
+                                    aria-hidden="true"
+                                  />
+                                </Disclosure.Button>
+                                <Disclosure.Panel as="ul" className="mt-1 px-2">
+                                  {item.children.map((subItem) => (
+                                    <li key={subItem.name}>
+                                      {/* 44px */}
+                                      <Disclosure.Button
+                                        as="a"
+                                        href={subItem.href}
+                                        className={classNames(
+                                          subItem.current ? 'bg-gray-50' : 'hover:bg-gray-100 hover:text-gray-900',
+                                          'block rounded-md py-2 pr-2 pl-9 text-sm leading-6 text-gray-400'
+                                        )}
+                                      >
+                                        {subItem.name}
+                                      </Disclosure.Button>
+                                    </li>
+                                  ))}
+                                </Disclosure.Panel>
+                              </>
+                            )}
+                          </Disclosure>
+                        )}
+                      </li>
+                    ))}
+                  </ul>
+                </li>
+                <li className="mt-auto">
+                  <a
+                    href="#"
+                    className="group -mx-2 flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6 text-gray-400 hover:bg-gray-100 hover:text-gray-900"
+                  >
+                    <Cog6ToothIcon className="h-6 w-6 shrink-0" aria-hidden="true" />
+                    Settings
+                  </a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+
+        <div className="lg:pl-72">
+          <div className="sticky top-0 z-40 flex h-16 shrink-0 items-center gap-x-4 border-b border-gray-200 bg-white px-4 shadow-sm sm:gap-x-6 sm:px-6 lg:px-8">
+            <button type="button" className="-m-2.5 p-2.5 text-gray-700 lg:hidden" onClick={() => setSidebarOpen(true)}>
+              <span className="sr-only">Open sidebar</span>
+              <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+            </button>
+
+            {/* Separator */}
+            <div className="h-6 w-px bg-gray-900/10 lg:hidden" aria-hidden="true" />
+
+            <div className="flex flex-1 gap-x-4 self-stretch lg:gap-x-6">
+              <SearchBar />
+              <Profile />
+            </div>
+          </div>
+
+          <main className="py-10">
+            <div className="px-4 sm:px-6 lg:px-8">{children}</div>
+          </main>
+        </div>
+      </div>
+    </>
+  )
+}
