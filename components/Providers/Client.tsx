b164770b884b63c09fedac2a077a4484c4cfc903
Ed Timmons <ed@delsurf.com>
2024-04-23 11:48:30 -0400

    move auth gate to Providers; create Server/Client providers

---
+++ components/Providers/Client.tsx
@@ -0,0 +1,9 @@
+'use client'
+
+import { NextUIProvider } from '@nextui-org/react'
+
+export function Client({children}: { children: React.ReactNode }) {
+    return <NextUIProvider>
+        {children}
+    </NextUIProvider>
+}
---
+++ components/Providers/Server.tsx
@@ -0,0 +1,7 @@
+import AuthProvider from "../Auth/Provider"
+
+export function Server({children}: { children: React.ReactNode }) {
+    return <AuthProvider>
+        {children}
+    </AuthProvider>
+}
