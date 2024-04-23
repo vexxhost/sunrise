9fd9e34b06234d68070f5d0a529e1c85f5458158
Ed Timmons <ed@delsurf.com>
2024-04-23 11:50:01 -0400

    show Login component for id provider input; implement server action with validation

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,19 +1,37 @@
-import { session } from "@/lib/session";
-import { redirect } from "next/navigation";
+import { z } from 'zod'
+import { redirect } from "next/navigation"
 
-export const startFederatedAuth = async (redirectTo?: string) => {
-  //await session().set("redirect_to", redirectTo || "/");
-  if (process.env.TOKEN) {
-    redirect('/auth/websso');
-  }
+const idProviders:string[]|undefined = process.env.KEYSTONE_FEDERATION_IDENTITY_PROVIDERS?.split(',')
+
+if (!idProviders || (idProviders.length == 1 && idProviders[0] == '')) {
+  throw new Error('No Identity Providers configured')
+}
+
+export const LoginFormSchema = z.object({
+    idProvider: z
+      .string().refine((value) => idProviders.includes(value), {
+        message: "Invalid Identity Provider"
+      })
+})
+
+export type LoginFormState = 
+    | {
+        errors?: {
+            idProvider?: string[]
+        }
+        message?: string
+      }
+    | undefined
+
+export const redirectToIdentityProvider = (idProvider:string) => {
   redirect(
     process.env.KEYSTONE_API +
       "/v3/auth/OS-FEDERATION/identity_providers/" +
-      process.env.KEYSTONE_FEDERATION_IDENTITY_PROVIDER +
+      idProvider +
       "/protocols/" +
       process.env.KEYSTONE_FEDERATION_IDENTITY_PROVIDER_PROTOCOL +
       "/websso?origin=" +
       process.env.DASHBOARD_URL +
       "/auth/websso/"
   );
-};
+}
