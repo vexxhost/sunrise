f2c97c2c4951ff55a9e5c33ca95a479be9a60893
Mohammed Naser <mnaser@vexxhost.com>
2025-11-02 14:33:51 -0500

    added shadcn
    Signed-off-by: Mohammed Naser <mnaser@vexxhost.com>

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,8 +1,8 @@
-import { type ClassValue, clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
+  return twMerge(clsx(inputs))
 }
 
 export function capitalize(str: string) {
@@ -15,7 +15,7 @@ export function formatMBtoGB(sizeInMb: number) {
 }
 
 export function capitalizeFirstLetters(str: string) {
-  const separators = ['-', ',', '|', '_','-'];
+  const separators = ['-', ',', '|', '_', '-'];
   const regexPattern = new RegExp(`[${separators.join('')}]`, 'g'); // Create a regular expression pattern with the given separators
   return str
     .split(regexPattern) // Split the string using the separators
