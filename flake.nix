bf2904516a88902d7ed626966736ab79d3bc36a8
Mohammed Naser <mnaser@vexxhost.com>
2025-11-02 13:00:00 -0500

    add flakes
    Signed-off-by: Mohammed Naser <mnaser@vexxhost.com>

--- flake.nix
+++ flake.nix
@@ -1,23 +1,57 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    devshell = {
+      url = "github:numtide/devshell";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem
-      (system:
-        let
-          pkgs = import nixpkgs {
-            inherit system;
-          };
-        in
+  outputs =
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        inputs.treefmt-nix.flakeModule
+        inputs.devshell.flakeModule
+      ];
+
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
+      perSystem =
         {
-          devShell = pkgs.mkShell {
-            buildInputs = with pkgs; [
-              nodejs
-            ];
+          config,
+          self',
+          inputs',
+          pkgs,
+          system,
+          ...
+        }:
+        {
+          treefmt = {
+            programs = {
+              nixfmt.enable = true;
+            };
           };
-        }
-      );
+
+          devshells.default = {
+            packages =
+              with pkgs;
+              [
+                nodejs
+              ]
+              ++ (builtins.attrValues config.treefmt.build.programs);
+          };
+        };
+    };
 }
